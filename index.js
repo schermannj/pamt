840e0dd60ac3ad84879af5217fc30cdc83812bf7
schrmnn <schermannj23@gmail.com>
2018-08-25 12:25:51 +0300

    refactored, removed extra steps

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 const { TYPES } = require('./constants');
 
-const pamt = mod => new Proxy(mod, {
+const def = definitions => mod => new Proxy(mod, {
   get: function (target, prop) {
-    const fn = target[prop];
+    const val = target[prop];
+    const definition = definitions[prop];
 
-    if (typeof fn !== 'function') return fn;
-    if (!fn.__pamt) return fn;
+    if (!definition || typeof val !== 'function') return val;
 
     return function pamtProxyGet(...params) {
       const err = params
-        .map((it, i) => typeof it !== fn.__pamt[i] && new Error(`Invalid arg type of ${it}. It has to be ${fn.__pamt[i]}`))
+        .map((it, i) => typeof it !== definition[i] && new Error(`Invalid arg type of ${it}. It has to be ${definition[i]}`))
         .find(it => it);
 
       if (err) throw err;
 
-      return fn(...params);
+      return val(...params);
     };
   }
 });
-
-const def = definitions => mod => {
-  Object
-    .keys(mod)
-    .filter(it => typeof mod[it] === 'function') // TODO: use get
-    .forEach(it => (mod[it].__pamt = definitions[it]));
-
-  return pamt(mod);
-};
 
 module.exports = {
   def,
